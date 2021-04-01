e01d026590a6d6aab8cdbb0729372b3d570d8fc6
Peter Rukin <61048011+RukInDaHouse@users.noreply.github.com>
2021-04-01 16:06:57 +0700

    add darkness and politic

--- js/script.js
+++ js/script.js
@@ -15,10 +15,21 @@ for (i = 0; i < acc.length; i++) {
 
 document.getElementById("modal__button").addEventListener("click", function() {
   var modal = document.getElementById("modal");
+  var gray__overlay = document.getElementById("gray__overlay");
       modal.classList.toggle("open__menu");
+      gray__overlay.classList.toggle("gray__overlay");
 });
 
 document.getElementById("modal__arrow").addEventListener("click", function() {
   var modal = document.getElementById("modal");
+  var gray__overlay = document.getElementById("gray__overlay");
       modal.classList.toggle("open__menu");
+      gray__overlay.classList.toggle("gray__overlay");
+});
+
+document.getElementById("submit").addEventListener("click", function() {
+  var modal = document.getElementById("modal");
+  var gray__overlay = document.getElementById("gray__overlay");
+      modal.classList.toggle("open__menu");
+      gray__overlay.classList.toggle("gray__overlay");
 });
